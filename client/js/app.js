392c2f8e41ad7f408d9fb99db8687dcf7d639802
Razvan Moraru <razvan@inclub.ro>
2015-07-09 16:48:58 +0200

    Added lib.js and app.js in order to make the application more modular

--- client/js/app.js
+++ client/js/app.js
@@ -1,37 +1,35 @@
-API_URL = window.location.href + 'rest/';
+API_URL = window.location.href.replace('#','') + 'rest/';
+var APP = {
+    exploreLinks: function(elem) {
+        console.log('ExploreLinks');
+        LIB.ajax('GET', API_URL + 'sites').then(function(r) {
+            console.log('Results', r);
+        }, function(err) {
+            console.log('Err', err);
+        });
+    },
+    explorePosts: function(elem) {
+        console.log('ExplorePOSts');
+        LIB.ajax('GET', API_URL + 'posts').then(function(r) {
+            console.log('Results', r);
+        }, function(err) {
+            console.log('Err', err);
+        });
+    },
+    exploreCategories: function(elem) {
+        console.log('Explore Categories');
+        LIB.ajax('GET', API_URL + 'categories').then(function(r) {
+            console.log('Results', r);
+        }, function(err) {
+            console.log('Err', err);
+        });
+    },
+};
 
-function ajax(method, url, async) {
-    var timeout;
-    return promise = new Promise(function(resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open(method, url, async || true);
 
-        xhr.onload = function(e) {
-            if (this.status === 200) {
-                clearTimeout(timeout);
-                results = JSON.parse(this.responseText);
-                resolve(results);
-            }
-        };
+var actions = document.querySelectorAll('[data-action]');
 
-        xhr.onerror = function(e) {
-            console.log('Rejected');
-            clearTimeout(timeout);
-            reject(e);
-        };
-
-        xhr.send();
-
-        timeout = setTimeout(function() {
-            xhr.abort();
-            reject('Error API');
-        }, 2000);
-
-    });
-}
-
-ajax('GET', API_URL + 'sites').then(function(r) {
-    console.log('Results', r);
-}, function(err) {
-    console.log('Err', err);
+Array.prototype.map.call(actions, function(elem, idx) {
+    var action = elem.dataset.action;
+    elem.addEventListener('click', APP[action], false);
 });
---
+++ client/js/lib.js
@@ -0,0 +1,33 @@
+var LIB = {
+    ajax: function ajax(method, url, async) {
+        var timeout;
+    console.log('url', url);
+        return promise = new Promise(function(resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.open(method, url, async || true);
+
+            xhr.onload = function(e) {
+                if (this.status === 200) {
+                    clearTimeout(timeout);
+                    results = JSON.parse(this.responseText);
+                    resolve(results);
+                }
+            };
+
+            xhr.onerror = function(e) {
+                console.log('Rejected');
+                clearTimeout(timeout);
+                reject(e);
+            };
+
+            xhr.send();
+
+            timeout = setTimeout(function() {
+                xhr.abort();
+                reject('Error API');
+            }, 2000);
+
+        });
+    }
+
+};
